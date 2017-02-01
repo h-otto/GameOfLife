63ac7c8ecf02617d86725bae9adad7bbdbeb2079
Havasvölgyi Ottó <havasvolgyi.otto.gt@gmail.com>
2017-02-01 10:48:26 +0100

    Generáció léptető, és tesztek hozzá

---
+++ src/GameOfLife.Core/Stepper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Core
+{
+    /// <summary>
+    /// Egy generációból létrehoz egy újat
+    /// </summary>
+    public class Stepper
+    {
+        static readonly List<Point> Neighbours = new List<Point>
+        {
+            new Point(-1,-1), new Point(-1,0), new Point(-1,1), new Point(0,1),
+            new Point(1,1), new Point(1,0), new Point(1,-1), new Point(0,-1),
+        };
+
+        public bool[,] GetNextGeneration(bool[,] initial)
+        {
+            if (initial == null)
+                throw new ArgumentNullException(nameof(initial));
+
+            int height = initial.GetLength(0);
+            int width = initial.GetLength(1);
+
+            var neighbourCounts = new int[height, width];
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    for (int i = 0; i < Neighbours.Count; i++)
+                    {
+                        var neighbourPosition = new Point(col + Neighbours[i].X, row + Neighbours[i].Y);
+                        if (neighbourPosition.X >= 0 && neighbourPosition.Y >= 0
+                            && neighbourPosition.X < width && neighbourPosition.Y < height)
+                        {
+                            neighbourCounts[row, col]++;
+                        }
+                    }
+                }
+            }
+
+            //az előző generációból indulunk ki
+            bool[,] nextGen = (bool[,])initial.Clone();
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    if (neighbourCounts[row, col] < 2 || neighbourCounts[row, col] > 3)
+                        //meghal
+                        nextGen[row, col] = false;
+
+                    else if (neighbourCounts[row, col] == 3)
+                        //új keletkezik
+                        nextGen[row, col] = true;
+
+                }
+            }
+
+            return nextGen;
+        }
+    }
+}
---
+++ src/GameOfLife.Tests/StepperTests.cs
@@ -0,0 +1,149 @@
+﻿using GameOfLife.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameOfLife.Tests
+{
+    public class StepperTests
+    {
+        [Fact]
+        public void EmptyBoard_0_0_Test()
+        {
+            var gen0 = new bool[0, 0];
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            Assert.Equal(0, gen1.GetLength(0));
+            Assert.Equal(0, gen1.GetLength(1));
+        }
+
+        [Fact]
+        public void EmptyBoard_5_0_Test()
+        {
+            var gen0 = new bool[5, 0];
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            Assert.Equal(5, gen1.GetLength(0));
+            Assert.Equal(0, gen1.GetLength(1));
+        }
+
+        [Fact]
+        public void EmptyBoard_1_1_Test()
+        {
+            var gen0 = new bool[1, 1];
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            Assert.Equal(1, gen1.GetLength(0));
+            Assert.Equal(1, gen1.GetLength(1));
+
+            Assert.False(gen1[0, 0]);
+        }
+
+        [Fact]
+        public void FullBoard_1_1_Test()
+        {
+            var gen0 = new bool[1, 1];
+            gen0[0, 0] = true;
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            Assert.False(gen1[0, 0]);
+        }
+
+        [Fact]
+        public void FullBoard_2_1_Test()
+        {
+            var gen0 = new bool[2, 1];
+            gen0[0, 0] = true;
+            gen0[1, 0] = true;
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            Assert.False(gen1[0, 0]);
+            Assert.False(gen1[1, 0]);
+        }
+
+        [Fact]
+        public void FullBoard_2_2_Test()
+        {
+            var gen0 = new bool[2, 2];
+            gen0[0, 0] = true;
+            gen0[0, 1] = true;
+            gen0[1, 0] = true;
+            gen0[1, 1] = true;
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            Assert.True(gen1[0, 0]);
+            Assert.True(gen1[0, 1]);
+            Assert.True(gen1[1, 0]);
+            Assert.True(gen1[1, 1]);
+        }
+
+        [Fact]
+        public void Board_2x2_3Lives_Test()
+        {
+            var gen0 = new bool[2, 2];
+            gen0[0, 0] = true;
+            gen0[0, 1] = true;
+            gen0[1, 0] = true;
+            gen0[1, 1] = false;
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            //mindnem igazzá kell válnia
+            Assert.True(gen1[0, 0]);
+            Assert.True(gen1[0, 1]);
+            Assert.True(gen1[1, 0]);
+            Assert.True(gen1[1, 1]);
+        }
+
+        [Fact]
+        public void Board_3x3_9Lives_Test()
+        {
+            var gen0 = new bool[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    gen0[i, j] = true;
+                }
+            }
+
+            var stepper = new Stepper();
+
+            var gen1 = stepper.GetNextGeneration(gen0);
+
+            //sarkok maradnak, többi meghal
+            Assert.True(gen1[0, 0]);
+            Assert.False(gen1[0, 1]);
+            Assert.True(gen1[0, 2]);
+            Assert.False(gen1[1, 0]);
+            Assert.False(gen1[1, 1]);
+            Assert.False(gen1[1, 2]);
+            Assert.True(gen1[2, 0]);
+            Assert.False(gen1[2, 1]);
+            Assert.True(gen1[2, 2]);
+        }
+    }
+}
